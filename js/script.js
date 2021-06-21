93a9b3aeb80ca3a7c2300d23c1b24d07c6227edd
Akasha <Akasha.Rojee@gmail.com>
2021-06-22 00:31:40 +0400

    Refactor code to open/close mobile menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-const mobileMenuIcon = document.querySelector(".mobil-menu-icon");
-const mobileMenuClose = document.querySelector(".mobile-menu-close");
+const hamburgerButton = document.querySelector("nav button");
+const menu = document.querySelector(".menu");
+const closeButton = menu.querySelector("button");
+let activeMenuButton = document.querySelector(".menu-button");
+let menuItems;
 
-mobileMenuIcon.addEventListener("click", (e) => {
-  document.getElementById("mobile-menu").classList.add("display-unset");
-});
+//When mobile menu is expanded/collapsed, style menu items in h3/span respectively
+function styleMenuItems(newElement, oldElement) {
+  menuItems = menu.querySelectorAll(oldElement);
+  menuItems.forEach(menuItem => {
+    const element = document.createElement(newElement);
+    element.textContent = menuItem.textContent;
+    menuItem.replaceWith(element);
+  });
+}
 
-mobileMenuClose.addEventListener("click", (e) => {
-  document.getElementById("mobile-menu").classList.remove("display-unset");
-});
+// When mobile menu is collapsed/expanded, the active menu button (i.e. the button that should receive action) is the hamburger/close button respectively.
+function switchActiveMenuButton() {
+  menu.classList.toggle("mobile-menu");
+  activeMenuButton.classList.toggle("menu-button");
+  if (activeMenuButton == closeButton) {
+    hamburgerButton.classList.toggle("menu-button");
+    styleMenuItems("span", "h3");
+  } else {
+    closeButton.classList.toggle("menu-button");
+    styleMenuItems("h3", "span");
+  }
+  activeMenuButton = document.querySelector(".menu-button")
+  activeMenuButton.addEventListener("click", switchActiveMenuButton);
+}
+
+activeMenuButton.addEventListener("click", switchActiveMenuButton);
+
