744ca7f9aa2e324d551bc744242c03871f5b4aba
Akasha <Akasha.Rojee@gmail.com>
2021-06-26 01:05:44 +0400

    Fix linter errors

--- js/form.js
+++ js/form.js
@@ -4,23 +4,21 @@ const form = document.forms[0];
 const submitButton = form['user-submit'];
 const formInputs = [form.name, form.email, form.message];
 
-
 // When user clicks Submit button, submit form if all form inputs are valid,
 // Else display custom error message
 submitButton.addEventListener('click', (event) => {
-
   removeElement(form.querySelector('span'));
   event.preventDefault(); // Prevent default form validation messages
 
-  let invalidInput = validateInputs(formInputs);
+  const invalidInput = validateInputs(formInputs);
   if (invalidInput === -1) {
     form.submit();
   } else {
     const errorMessage = [
-      'Please enter your name', 
+      'Please enter your name',
       'Email should be lower case in format user@mail.com',
       'Please enter your message'];
-    let span = createElement('span', 'error', {}, errorMessage[invalidInput]);
+    const span = createElement('span', 'error', {}, errorMessage[invalidInput]);
     document.querySelector('form div').append(span);
   }
 });
@@ -30,21 +28,22 @@ addEventListeners(formInputs, 'click', () => {
   removeElement(form.querySelector('span'));
 });
 
-
 // When user changes content in any form input, save data to local storage
 addEventListeners(formInputs, 'change', () => {
   localStorage.setItem('form', JSON.stringify(
-    {name : formInputs[0].value, 
-    email : formInputs[1].value,
-    message : formInputs[2].value}));
-  }
-);
+    {
+      name: formInputs[0].value,
+      email: formInputs[1].value,
+      message: formInputs[2].value,
+    },
+  ));
+});
 
-//When page loads, populate form inputs with data from local storage
+// When page loads, populate form inputs with data from local storage
 function populateForm() {
   if (localStorage.length > 0) {
-    let formData = JSON.parse(localStorage.getItem('form'));
-    formInputs.forEach(formInput => {
+    const formData = JSON.parse(localStorage.getItem('form'));
+    formInputs.forEach((formInput) => {
       formInput.value = formData[formInput.id];
     });
   }
