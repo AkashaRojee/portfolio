b06f9beb33f7d563a7e23a2690b949dabde8ef55
yahyaelganyni1 <crazy20046@gmail.com>
2021-06-25 15:02:44 +0200

    Add event listeners to the form file

--- js/form.js
+++ js/form.js
@@ -4,10 +4,7 @@ const form = document.forms[0];
 const submitButton = form['user-submit'];
 let span = '';
 const [...formInputs] = [form.name, form.email, form.message];
-const [...errorMessage] = [
-  'Please enter your name',
-  'Email should be lower case in format user@mail.com',
-  'Please enter your message'];
+const [...errorMessage] = ['Please enter your name', 'Email should be lower case in format user@mail.com', 'Please enter your message'];
 
 // When user clicks Submit button, submit form if all form inputs are valid,
 // Else display custom error message
@@ -25,8 +22,11 @@ submitButton.addEventListener('click', (event) => {
 });
 
 // Remove error message when user clicks on any form input
-addEventListeners(formInputs,
-  'click',
-  () => {
-    removeElement(form.querySelector('span'));
-  });
+addEventListeners(formInputs, 'click', () => {
+  removeElement(form.querySelector('span'));
+});
+
+addEventListeners(formInputs, 'change', () => {
+  localStorage.setItem('name', formInputs.value);
+  console.log(localStorage);
+});
